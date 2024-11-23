4c0b84de64375f5d223471bc71b9c10559fce488
Maxim <334890@gmail.com>
2024-11-23 19:30:42 +0600

    Revert "rm migrate page"
    This reverts commit d5f4d6ecaadd024dd7a89f2b0941661c741c84d0.

--- src/drizzle/migrate.ts
+++ src/drizzle/migrate.ts
@@ -1,13 +1,38 @@
+"use server";
+
+// import "server-only";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 
-const migrationClient = new Database(process.env.DATABASE_URL || "", {
-  fileMustExist: false,
-});
+export async function requestMigration(formData: FormData) {
+  const correctPassphrase = process.env.MIGRATION_PASSPHRASE;
+  if (!correctPassphrase) {
+    console.error("MIGRATION_PASSPHRASE is not set");
+    return;
+  }
 
-migrate(drizzle(migrationClient), {
-  migrationsFolder: "./src/drizzle/migrations",
-});
-migrationClient.close();
+  const passphrase = formData.get("passphrase") as string;
+  if (passphrase !== correctPassphrase) {
+    console.error("Incorrect passphrase");
+    return;
+  }
+
+  console.log("Attempting migration.......................");
+
+  attemptMigration();
+}
+
+function attemptMigration() {
+  const migrationClient = new Database(process.env.DATABASE_URL || "", {
+    fileMustExist: false,
+  });
+
+  migrate(drizzle(migrationClient), {
+    migrationsFolder: "./src/drizzle/migrations",
+  });
+  migrationClient.close();
+}
+
+attemptMigration();
--- src/drizzle/migrations/0008_big_sister_grimm.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `tasks` ADD `test3` text;--> statement-breakpoint
-ALTER TABLE `tasks` DROP COLUMN `test2`;
