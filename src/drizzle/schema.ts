f0df7f99fbc5680ea3642157bcbf535f40ecef10
Maxim <334890@gmail.com>
2024-11-23 20:13:18 +0600

    add another column

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -14,6 +14,7 @@ export const TaskTable = sqliteTable("tasks", {
     .notNull(),
   created_at: text("created_at").$defaultFn(isoDate).notNull(),
   updated_at: text("updated_at").$onUpdateFn(isoDate).notNull(),
+  test7: text("test7"),
 });
 
 export type InsertTask = InferInsertModel<typeof TaskTable>;
