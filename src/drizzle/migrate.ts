d5f4d6ecaadd024dd7a89f2b0941661c741c84d0
Maxim <334890@gmail.com>
2024-11-23 19:22:11 +0600

    rm migrate page

--- src/drizzle/migrate.ts
+++ src/drizzle/migrate.ts
@@ -1,38 +1,13 @@
-"use server";
-
-// import "server-only";
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import Database from "better-sqlite3";
 
-export async function requestMigration(formData: FormData) {
-  const correctPassphrase = process.env.MIGRATION_PASSPHRASE;
-  if (!correctPassphrase) {
-    console.error("MIGRATION_PASSPHRASE is not set");
-    return;
-  }
+const migrationClient = new Database(process.env.DATABASE_URL || "", {
+  fileMustExist: false,
+});
 
-  const passphrase = formData.get("passphrase") as string;
-  if (passphrase !== correctPassphrase) {
-    console.error("Incorrect passphrase");
-    return;
-  }
-
-  console.log("Attempting migration.......................");
-
-  attemptMigration();
-}
-
-function attemptMigration() {
-  const migrationClient = new Database(process.env.DATABASE_URL || "", {
-    fileMustExist: false,
-  });
-
-  migrate(drizzle(migrationClient), {
-    migrationsFolder: "./src/drizzle/migrations",
-  });
-  migrationClient.close();
-}
-
-attemptMigration();
+migrate(drizzle(migrationClient), {
+  migrationsFolder: "./src/drizzle/migrations",
+});
+migrationClient.close();
---
+++ src/drizzle/migrations/0008_big_sister_grimm.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `tasks` ADD `test3` text;--> statement-breakpoint
+ALTER TABLE `tasks` DROP COLUMN `test2`;
