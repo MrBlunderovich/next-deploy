30bd39d08279ec49850dba9dc56b71c2deb5671b
Maxim <334890@gmail.com>
2024-12-29 17:01:58 +0600

    add homepage link

--- src/app/(private)/admin/cms/homepage/page.tsx
+++ src/app/(private)/admin/cms/homepage/page.tsx
@@ -1,12 +1,22 @@
 import { cachedHomepageBannerSection } from "@/app/(private)/admin/cms/homepage/actions";
 import BannerForm from "./_component/BannerForm";
+import { ExternalLinkIcon } from "lucide-react";
+import Link from "next/link";
 
 export default async function CmsHomepage() {
   const bannerData = await cachedHomepageBannerSection();
 
   return (
     <>
-      <h1 className="h1">Homepage</h1>
+      <h1 className="h1 flex items-center justify-center gap-4">
+        Homepage
+        <Link href={"/"}>
+          <ExternalLinkIcon
+            className="text-yellow-500 hover:opacity-80"
+            size={40}
+          />
+        </Link>
+      </h1>
       <BannerForm bannerData={bannerData} />
     </>
   );
